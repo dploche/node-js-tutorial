0a552b94cdfdedda53d645949ab148cfd42be9b2
Diego Ploche <128251684+dploche@users.noreply.github.com>
2023-07-10 17:21:34 -0500

    Async patterns and npm practice

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-//npm - global command, comes with node
-// npm --version
+const http = require('htpp');
 
-// local dependency - use for this particular project
-// npm
+const server = http.createServer((req,res) => {
 
-//global dependency - use in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+});
 
-// package.json - manifest file(stores important info about project/package)
-// manual approach (create package.json in the root)
-// npm init (step by step, press enter to skip)
-// npm init -y(everything default)
-
-const _ = require('lodash');
-
-const items = [1, [2, [3, [4]]]];
-
-const newItems = _.flattenDeep(items);
-
-console.log(newItems);
+server.listen(5500, () => {
+    console.log('Server listening on port 5500...');
+});
