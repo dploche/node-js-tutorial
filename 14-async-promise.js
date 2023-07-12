59f1c036ba0e0e2c008eea23246b0ade5fa129c2
Diego Ploche <diegoploche86@gmail.com>
2023-07-11 21:50:27 -0500

    Refactored async to use modules and do a better approach

--- 14-async-promise.js
+++ 14-async-promise.js
@@ -1,18 +1,41 @@
+// Best async approach
+
 const fs = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
 
-const getText = (path) => {
-    return new Promise((resolve, reject) =>
-    {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else{
-                resolve(data);
-            }
-        });
-    });
-};
+const start = async() => {
+    try {
+        const first = await  readFilePromise('./content/first.txt', 'utf-8');
+        const second = await readFilePromise('./content/second.txt', 'utf-8');
+        await writeFilePromise(
+            './content/result-mind-grenade.txt', 
+            'THIS IS AWESOME: ' + first + second //Can add flag as third argument
+            );
+        console.log(first, second);
+    } catch (error){
+        console.log(error);
+    }
+}
 
-getText('./content/first.txt')
-.then((result) => console.log(result))
-.catch((err) => console.log(err));
+start();
+
+// This is whats happening in the background || Not ideal approach
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) =>
+//     {
+//         fs.readFile(path, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else{
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+// getText('./content/first.txt');
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err));
